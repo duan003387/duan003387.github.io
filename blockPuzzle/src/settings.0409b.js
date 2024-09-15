e925e5a431523e7118fd8e9884be7e253ba68d91
duan jian bo <37204891+duan003387@users.noreply.github.com>
2024-09-15 11:40:14 +0800

    a

---
+++ blockPuzzle/src/settings.0409b.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/Main.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"f1413",resources:"667a1",main:"f0464"}};
